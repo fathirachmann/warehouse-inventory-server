255edd466497f8b8e87c48777ae31e035a8fcc38
fathirachmann <fathirachmann@gmail.com>
2025-12-04 21:01:36 +0700

    feat: Added gorm model for history_stok and mstok, updated relational database schema

--- models/history_stok.go
+++ models/history_stok.go
@@ -1,1 +1,20 @@
 package models
+
+import "time"
+
+type HistoryStok struct {
+	ID             uint      `gorm:"primaryKey;autoIncrement" json:"id"`
+	BarangID       uint      `gorm:"not null" json:"barang_id"`
+	UserID         uint      `gorm:"not null" json:"user_id"`
+	JenisTransaksi string    `gorm:"not null" json:"jenis_transaksi"` // "masuk" or "keluar"
+	Jumlah         int       `gorm:"not null" json:"jumlah"`
+	StokSebelumnya int       `gorm:"not null" json:"stok_sebelumnya"`
+	StokSesudah    int       `gorm:"not null" json:"stok_sesudah"`
+	Keterangan     string    `json:"keterangan"`
+	CreatedAt      time.Time `gorm:"autoCreateTime" json:"created_at"`
+	UpdatedAt      time.Time `gorm:"autoUpdateTime" json:"updated_at"`
+
+	// Associations
+	MasterBarang MasterBarang `gorm:"foreignKey:BarangID;references:ID" json:"master_barang"`
+	Users        User         `gorm:"foreignKey:UserID;references:ID" json:"user"`
+}
--- models/stok.go
+++ models/stok.go
@@ -1,1 +1,19 @@
 package models
+
+import (
+	"time"
+)
+
+type Mstok struct {
+	ID        uint      `gorm:"primaryKey" json:"id"`
+	BarangID  uint      `gorm:"not null" json:"barang_id"`
+	StokAkhir int       `gorm:"default:0" json:"stok_akhir"`
+	UpdatedAt time.Time `json:"updated_at"`
+
+	// Associations
+	MasterBarang MasterBarang `gorm:"foreignKey:BarangID;references:ID" json:"master_barang"`
+}
+
+func (Mstok) TableName() string {
+	return "mstok"
+}
